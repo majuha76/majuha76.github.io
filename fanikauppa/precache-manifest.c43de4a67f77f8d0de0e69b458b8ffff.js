be303211b21a84cab1ac76f976fc5f2131cebc07
Harjumäki Marko Juhani <marko.harjumaki@edu.tampere.fi>
2020-11-24 14:46:24 +0200

    fanikauppain

---
+++ fanikauppa/precache-manifest.c43de4a67f77f8d0de0e69b458b8ffff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8827a65b95b91e764769f6197c9948ae",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "94a47278313a192410c5",
+    "url": "/fanikauppa/static/css/main.4eadea69.chunk.css"
+  },
+  {
+    "revision": "c2e1ced98dba59139506",
+    "url": "/fanikauppa/static/js/2.fd2d9eab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.fd2d9eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a47278313a192410c5",
+    "url": "/fanikauppa/static/js/main.44001158.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  },
+  {
+    "revision": "8099011398cdc34d58252f99f88bc050",
+    "url": "/fanikauppa/static/media/hats1.80990113.png"
+  },
+  {
+    "revision": "02568246d58b9710b48224c2513e4dd4",
+    "url": "/fanikauppa/static/media/hats2.02568246.png"
+  },
+  {
+    "revision": "18dfed8263515620b8c90a787ef54851",
+    "url": "/fanikauppa/static/media/hats3.18dfed82.png"
+  }
+]);
